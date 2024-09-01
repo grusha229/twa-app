6ed9f39d7a2aba4002dea3e833b20ffa8f5bfff9
Grigory Tupikov <gtupikov@jetlend.ru>
2024-09-01 19:32:54 +0300

    upd welcome

--- src/views/Welcome/Welcome.tsx
+++ src/views/Welcome/Welcome.tsx
@@ -1,31 +1,36 @@
 import '@twa-dev/sdk';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Welcome.module.scss';
 import laptopGif from './../../assets/laptop--image.gif';
+import { retrieveLaunchParams } from '@tma.js/sdk-react';
 import WebApp from '@twa-dev/sdk';
+import { RootState } from '../../store';
+import { setInitDataRaw } from '../../store/features/authSlice';
 
 export default function Welcome() {
+  const dispatch = useDispatch();
 
-  // const { initDataRaw } = retrieveLaunchParams();
+  const { initDataRaw } = retrieveLaunchParams();
 
-  // const storedInitDataRaw = useSelector((state: RootState) => state.authentication.initDataRaw)
+  const storedInitDataRaw = useSelector((state: RootState) => state.authentication.initDataRaw)
 
-  // useEffect(() => {
-  //   dispatch(setInitDataRaw(initDataRaw));
-  // }, [initDataRaw]);
+  useEffect(() => {
+    dispatch(setInitDataRaw(initDataRaw));
+  }, [setInitDataRaw]);
 
-  // if (storedInitDataRaw) {
-  //   return <h2>Loading...</h2>
-  // }
+  if (storedInitDataRaw) {
+    return <h2>Loading...</h2>
+  }
 
   const onClick = useCallback(() => {
 
     WebApp.showPopup({
       title: 'Оп',
-      message: 'hey!',
+      message: storedInitDataRaw,
     })
 
-  },[])
+  },[storedInitDataRaw])
 
   return (
     <div className={styles['container']}>
